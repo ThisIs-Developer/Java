2b70e53ed783472ffe054018bf518fa67351b75c
ThisIs-Developer <baivabsarkar@gmail.com>
2024-08-31 23:04:38 +0530

    Updated pattern

---
+++ Pattern Printing/Striver 22/Pattern1.java
@@ -0,0 +1,28 @@
+/*
+ * Problem statement
+Sam is making a forest visualizer. An N-dimensional forest is represented by the pattern of size NxN filled with ‘*’.
+
+For every value of ‘N’, help sam to print the corresponding N-dimensional forest.
+
+Example:
+Input: ‘N’ = 3
+
+Output: 
+* * *
+* * *
+* * *
+ */
+
+import java.util.*;
+
+public class Pattern1{
+    public static void main(String[] args) {
+        int N = 3;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Pattern Printing/Striver 22/Pattern2.java
@@ -0,0 +1,30 @@
+/*
+ * Problem statement
+Sam is making a forest visualizer. An N-dimensional forest is represented by the pattern of size NxN filled with ‘*’.
+
+An N/2-dimensional forest is represented by the lower triangle of the pattern filled with ‘*’.
+
+For every value of ‘N’, help sam to print the corresponding N/2-dimensional forest.
+
+Example:
+Input:  ‘N’ = 3
+
+Output: 
+* 
+* *
+* * *
+ */
+
+ public class Pattern2 {
+    public static void nForest(int n) {
+        // Write your code here
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
